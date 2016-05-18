0f4f4beec329b15407927dbe473aeabf4b98ef3a
faisalmohd <faisalhmohd@live.com>
2016-05-18 18:38:52 +0600

    Initial files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$("body").click(function(){
+  $( ".caption" ).fadeOut( "slow", function() {
+    // Animation complete.
+  });
+  $( ".piece" ).fadeIn( "slow", function() {
+    // Animation complete.
+  });
+});
